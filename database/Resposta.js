b9300a3a569c71f1eef91edf2a6d98edfbaab269
mateusf93 <mateusferreirasilva@hotmail.com>
2022-11-07 21:24:01 -0300

    first commit

---
+++ database/Resposta.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize")
+const connection = require("./database")
+
+const Resposta = connection.define('Respostas',{
+    corpo:{
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    perguntaID:{
+        type: Sequelize.INTEGER,
+        allowNull:false
+    }
+});
+
+Resposta.sync({force:false});
+
+module.exports = Resposta;
+
